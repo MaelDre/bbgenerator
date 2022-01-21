7d7a92ef90652227f1eaa0e09ad4b80994723cb4
Maël <mael@bougetb.fr>
2022-01-21 10:18:13 +0100

    try with the app

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
 import os
-# from flask import Flask
+ from flask import Flask
 from BBGen import bbgenerator
 
-#appli = Flask(__name__)
+app = Flask(__name__)
 
+@app.route("/")
+def generate_buzz():
+    page = '<html><body>'
+    page += '<h1>Bonjour tout le monde! La on va voir oupas</h1><h2>'
+    #page += bbgenerator.generate_buzz()
+    page += '</h2></body></html>'
+    return page
 
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    body = ['<!DOCTYPE html><html><meta charset="utf-8">',
-            '<title>It works</title><h1>It works!</h1>']
-    return [line.encode('utf-8') for line in body]
-
-app = application
-
-#@appli.route("/")
-#def generate_buzz():
-#    page = '<html><body>'
-#    page += '<h1>Bonjour tout le monde! La on va voir oupas</h1><h2>'
-#    #page += bbgenerator.generate_buzz()
-#    page += '</h2></body></html>'
-#    return page
-
-#if __name__ == "__main__":
-#    appli.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+if __name__ == "__main__":
+    appli.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
